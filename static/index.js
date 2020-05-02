cadb229dcad080c7ecd0e4ffd5865d622527b9a7
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2020-05-02 15:48:57 +0100

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,10 +1,44 @@
 var navbar = document.querySelector("nav.navBar");
+var circular44 = document.querySelector("img.imgIn");
 
 var sticky = navbar.offsetTop;
 var sendOn = false;
 
-
 window.onscroll = function(){scrollFunct()};
+
+const startX44 = circular44.x;
+const startY44 = circular44.y;
+var maxMove = 5;
+var addIntoX;
+var addIntoY;
+window.addEventListener("mousemove", function(e){
+    
+    // if (circular44.x + e.movementX >= startX44+maxMove){
+    //     false
+    //     // circular44.style.left = addIntoX.toString()+"px";
+    // }
+    // if(circular44.y + e.movementY >= startY44+maxMove){
+    //     false
+    //     // circular44.style.left = addIntoX.toString()+"px";
+    // }
+    // if (circular44.x - e.movementX <= startX44-maxMove){
+    //     false
+    //     // circular44.style.left = addIntoX.toString()+"px";
+    // }
+    // if(circular44.y - e.movementY <= startY44-maxMove){
+    //     false
+    //     // circular44.style.left = addIntoX.toString()+"px";
+    // }
+    // else{
+        // circular44.style.left = (e.x-circular44.x).toString()+"px";
+        // circular44.style.top = (e.y-circular44.y).toString()+"px";
+        circular44.style.left = e.movementX*1.2.toString()+"px";
+        circular44.style.top = e.movementY*1.2.toString()+"px";
+
+        console.log(e.movementX.toString()+"px", e.movementY.toString()+"px")
+    // }
+});
+
 
 
 function scrollFunct(){
